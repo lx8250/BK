6fe7dbfe2d6c43a350462864f948609d8dd9bc18
lixin <lx8250@126.com>
2018-04-08 17:14:37 +0800

    2018-4-8日提交

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -2,6 +2,8 @@
 namespace app\admin\controller;
 use think\Controller;
 use think\Db;
+use think\Session;
+
 class Login extends Controller{
     public function login(){
 
@@ -10,9 +12,11 @@ class Login extends Controller{
             $password = md5(input('password'));
             $temp = Db::query("select * from bk_user where user_name = '$name' and user_pwd='$password'");
             if(empty($temp)){
-                return 'false';
+                $this->error('用户名或密码错误','/admin/login/login');
             }
-            return 'OK';
+            Session::set('user',$name,3600);
+            Session::set('passworld',$password,3600);
+            $this->success('登陆成功','/admin/index/index');
         }
         return $this->fetch();
     }
